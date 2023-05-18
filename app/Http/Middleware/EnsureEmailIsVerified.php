790ca120549baa918388669f6da890be0c4d48df
vagrant <vagrant@homestead>
2023-05-18 08:29:44 +0000

    强制用户认证

---
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class EnsureEmailIsVerified
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        /* 三个判断
+        1. 如果用户已经登录
+        2. 并且还未认证 Email
+        3. 并且访问的不是 Email 相关的 URL 或者退出的 URL */
+
+        if($request->user() && ! $request->user()->hasVerifiedEmail() && ! $request->is('email/*', 'logout')){
+            return $request->expectsJson() ? abort('403','Your Email address is not verified.') : redirect()->route('verification.notice');
+        }
+
+        return $next($request);
+    }
+}
